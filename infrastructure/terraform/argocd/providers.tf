dab8df6b9dd30f5dce86b05b1cbe204625abd665
loeken <loeken@internetz.me>
2023-08-13 00:33:28 +0200

    removed

--- infrastructure/terraform/argocd/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "helm" {
-    kubernetes {
-        host                   = "https://127.0.0.1:${var.tunnel_port}"
-        client_certificate     = base64decode(var.ClientCertificateData)
-        client_key             = base64decode(var.ClientKeyData)
-        cluster_ca_certificate = base64decode(var.CertificateAuthorityData)
-    }
-}
--- infrastructure/terraform/k3s/ssh_config.tpl
+++
@@ -1,6 +0,0 @@
-%{ for config in configs ~}
-Host ${config.host}
-    User ${config.user}
-    IdentityFile ${config.identityfile}
-    ProxyJump ${config.proxyjump}
-%{ endfor ~}
